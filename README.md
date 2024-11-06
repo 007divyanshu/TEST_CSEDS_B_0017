566e2747fa4b599a2d2ecca2abf471572615a481
007divyanshu <pandeydivyanshu1509@gmail.com>
2024-11-06 14:38:39 +0530

    Joke Generator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TEST_DS_B_0017
+Random Joke Generator 
+Character Counter
---
+++ script.js
@@ -0,0 +1,19 @@
+async function getJoke() {
+    const response = await fetch('https://v2.jokeapi.dev/joke/Any');
+    const data = await response.json();
+
+    let joke = '';
+    if (data.type === 'single') {
+        joke = data.joke;
+    } else {
+        joke = `${data.setup} ${data.delivery}`;
+    }
+
+    document.getElementById('joke').innerText = joke;
+    document.getElementById('character-count').innerText = `Character count: ${joke.length}`;
+}
+
+function clearJoke() {
+    document.getElementById('joke').innerText = 'Press the button for a joke!';
+    document.getElementById('character-count').innerText = 'Character count: 0';
+}
